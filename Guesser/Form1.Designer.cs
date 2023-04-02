0e0c6a3d5fc6da79fdb60e7f0534350db71021a6
Kacper Karabinowski <kackar87@wp.pl>
2023-04-02 21:14:07 +0200

    version 1.0 //working version

--- Guesser/Form1.Designer.cs
+++ Guesser/Form1.Designer.cs
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblGenerateInfo = new System.Windows.Forms.Label();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.tbGuess = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(60)))), ((int)(((byte)(70)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnGenerate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(33, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(20);
+            this.panel1.Size = new System.Drawing.Size(648, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(24, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "From 0 to 100:)";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(501, 37);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(124, 25);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(23, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generate a random number and guess:)";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(60)))), ((int)(((byte)(70)))));
+            this.panel2.Controls.Add(this.btnCheck);
+            this.panel2.Controls.Add(this.lblGenerateInfo);
+            this.panel2.Controls.Add(this.lblClose);
+            this.panel2.Controls.Add(this.tbGuess);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(33, 177);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(20);
+            this.panel2.Size = new System.Drawing.Size(648, 271);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(501, 101);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(124, 25);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblGenerateInfo
+            // 
+            this.lblGenerateInfo.AutoSize = true;
+            this.lblGenerateInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenerateInfo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblGenerateInfo.Location = new System.Drawing.Point(23, 226);
+            this.lblGenerateInfo.Name = "lblGenerateInfo";
+            this.lblGenerateInfo.Size = new System.Drawing.Size(0, 25);
+            this.lblGenerateInfo.TabIndex = 3;
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblClose.Location = new System.Drawing.Point(23, 101);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(0, 25);
+            this.lblClose.TabIndex = 2;
+            // 
+            // tbGuess
+            // 
+            this.tbGuess.Location = new System.Drawing.Point(501, 26);
+            this.tbGuess.Name = "tbGuess";
+            this.tbGuess.Size = new System.Drawing.Size(124, 20);
+            this.tbGuess.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(23, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(283, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter your number to guess:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(714, 481);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(30);
+            this.Text = "Guesser";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.TextBox tbGuess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblGenerateInfo;
     }
 }
 
--- Guesser/Form1.cs
+++ Guesser/Form1.cs
@@ -16,5 +16,39 @@ namespace Guesser
         {
             InitializeComponent();
         }
+
+        Random random = new Random();
+        int number = 0;
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.Enabled = false;
+            button.BackColor = Color.Gray;
+            lblGenerateInfo.Text = "Number has been generated";
+            number = random.Next(100);        
+        }
+
+     
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(tbGuess.Text) == number)
+            {
+                lblClose.Text = "You won:)";
+                lblClose.ForeColor = Color.Green;
+                MessageBox.Show("Congratulations!", "You won:)", MessageBoxButtons.OK);
+            }
+            else if (int.Parse(tbGuess.Text) > number)
+            {
+                lblClose.Text = "The number is smaller";
+                lblClose.ForeColor = Color.YellowGreen;
+            }
+            else if (int.Parse(tbGuess.Text) < number)
+            {
+                lblClose.Text = "The number is bigger";
+                lblClose.ForeColor = Color.YellowGreen;
+            }
+        }
     }
 }
